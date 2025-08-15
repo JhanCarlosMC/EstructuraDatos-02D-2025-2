19b034b7122650909878c1ec7f94c5dca5356ff0
JhanCarlosMC <jcmartinez@uniquindio.edu.co>
2025-08-15 08:52:47 -0500

    Feat: Ejercicios de recursividad y divide y venceras

--- RepasoJavaRecursividad/src/main/java/co/edu/uniquindio/Main.java
+++ RepasoJavaRecursividad/src/main/java/co/edu/uniquindio/Main.java
@@ -1,7 +1,66 @@
-package co.edu.uniquindio;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+//package co.edu.uniquindio;
+//
+//import java.util.List;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        //estructuras condicionales
+//        if(condicion){
+//            //En caso de verdad
+//        }
+//
+//        if(condicion){
+//            //En caso de verdad
+//        }else{
+//            //En caso de falso
+//        }
+//
+//        if(condicion){
+//            //En caso de verdad
+//        }else if(condicion2){
+//            //En caso de verdadero2
+//        }
+//
+//        switch (option){
+//
+//            case 1:
+//                metodo;
+//                break;
+//            case 2:
+//                metodo;
+//                break;
+//
+//                default:
+//                    break;
+//        }
+//
+//        try {
+//
+//        }catch(Exception e){
+//
+//        }
+//
+//        //Estruturas ciclicas
+//        for (int i = 0; i < 10; i++){
+//        }
+//
+//        while (true){
+//        }
+//
+//        do{
+//        }while (true);
+//
+//
+//    }
+//
+//    public int encontrarMayorImperativo(List<Integer> lista){
+//        int mayor = 0;
+//        for (Integer num:lista){
+//            if (num > mayor){
+//                mayor = num;
+//            }
+//        }
+//        return mayor;
+//    }
+//}
